3472b9e4fd6a7f6e2985caf78e550c1201cfd1a1
Rishu123-png <jaswarrishu642@gmail.com>
2025-11-09 16:17:48 +0530

    Add initial students data to students.js
    Initialized studentsData array with student objects.

---
+++ students.js
@@ -0,0 +1,9 @@
+// students.js
+const studentsData = [
+  { name: "Rishu Jaswar", class: "11A", subject: "AI" },
+  { name: "Aarav Singh", class: "11B", subject: "CS" },
+  { name: "Meera Kapoor", class: "11A", subject: "Psychology" },
+  { name: "Raj Malhotra", class: "11C", subject: "Data Science" },
+  { name: "Ananya Gupta", class: "11B", subject: "PED" },
+  // 👆 You can add more students here
+];
