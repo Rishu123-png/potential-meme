4ef72ee8a8da1f58355eb951193846a4c1cf0122
Rishu123-png <jaswarrishu642@gmail.com>
2025-11-16 11:48:40 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,9 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+// firebase.js (npm version)
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
+// Your Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBSWzs19870cWmGxd9-kJsKOOs755jyuU0",
   authDomain: "school-attendence-system-9090.firebaseapp.com",
@@ -13,7 +14,9 @@ const firebaseConfig = {
   appId: "1:728832169882:web:b335869779e73ab8c20c23"
 };
 
-// Initialize Firebase
+// Initialize Firebase app
 export const app = initializeApp(firebaseConfig);
+
+// Firebase services
 export const auth = getAuth(app);
 export const db = getDatabase(app);
